91e21619417aa8ed9897229c685d4b9ba46291fa
Goran Ehrsson <goran@technipelago.se>
2013-06-22 15:38:11 +0200

    Added code-coverage

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,19 +20,24 @@ grails.project.dependency.resolution = {
         mavenRepo "http://labs.technipelago.se/repo/plugins-releases-local/"
     }
     dependencies {
+        //test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:2.2.0") {
+                ":release:2.2.1") {
             export = false
         }
-        test(":hibernate:$grailsVersion") {
+        runtime(":hibernate:$grailsVersion") {
             export = false
         }
 
-        test(":spock:0.7") { export = false }
-        test(":codenarc:0.17") { export = false }
+        test(":spock:0.7") {
+            export = false
+            //exclude "spock-grails-support"
+        }
+        test(":codenarc:0.18.1") { export = false }
+        test(":code-coverage:1.2.6") { export = false }
 
         compile("grails.crm:crm-core:latest.integration")
 
@@ -43,14 +48,28 @@ grails.project.dependency.resolution = {
 codenarc {
     reports = {
         CrmXmlReport('xml') {
-            outputFile = 'CodeNarcReport.xml'
+            outputFile = 'target/test-reports/CodeNarcReport.xml'
             title = 'Grails CRM CodeNarc Report'
         }
         CrmHtmlReport('html') {
             outputFile = 'target/test-reports/CodeNarcReport.html'
             title = 'Grails CRM CodeNarc Report'
+
         }
+    }
+    properties = {
+        GrailsPublicControllerMethod.enabled = false
+        CatchException.enabled = false
+        CatchThrowable.enabled = false
+        ThrowException.enabled = false
+        ThrowRuntimeException.enabled = false
+        GrailsStatelessService.enabled = false
+        GrailsStatelessService.ignoreFieldNames = "dataSource,scope,sessionFactory,transactional,*Service,messageSource,grailsApplication,applicationContext,expose"
     }
     processTestUnit = false
     processTestIntegration = false
+}
+
+coverage {
+    exclusions = ['**/radar/**']
 }
