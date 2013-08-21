18e8425bd17218c2452d45cd8284d906523fc02d
Goran Ehrsson <goran@technipelago.se>
2013-08-22 00:10:20 +0200

    Upgraded Grails to 2.2.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,47 +1,40 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.repos.default = "crm"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
     inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
     }
     log "warn"
+    legacyResolve false
     repositories {
-        grailsHome()
         grailsCentral()
         mavenRepo "http://labs.technipelago.se/repo/crm-releases-local/"
         mavenRepo "http://labs.technipelago.se/repo/plugins-releases-local/"
+        mavenCentral()
     }
     dependencies {
-        //test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
         build(":tomcat:$grailsVersion",
-                ":release:2.2.1") {
+                ":hibernate:$grailsVersion",
+                ":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
-        runtime(":hibernate:$grailsVersion") {
-            export = false
-        }
-
         test(":spock:0.7") {
             export = false
-            //exclude "spock-grails-support"
+            exclude "spock-grails-support"
         }
         test(":codenarc:0.18.1") { export = false }
         test(":code-coverage:1.2.6") { export = false }
 
-        compile("grails.crm:crm-core:latest.integration")
+        compile("grails.crm:crm-core:1.2.0")
 
-        runtime ":selection:latest.integration"
+        runtime ":selection:0.9.2"
     }
 }
 
