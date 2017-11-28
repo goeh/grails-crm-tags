00d68535074525b797121ad04ba880a7ca367ef9
Goran Ehrsson <goran@technipelago.se>
2017-11-28 18:49:17 +0100

    Next snapshot version

--- CrmTagsGrailsPlugin.groovy
+++ CrmTagsGrailsPlugin.groovy
@@ -18,7 +18,7 @@ import org.codehaus.groovy.grails.commons.GrailsClassUtils
 
 class CrmTagsGrailsPlugin {
     def groupId = ""
-    def version = "2.4.2"
+    def version = "2.4.3-SNAPSHOT"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCore']
